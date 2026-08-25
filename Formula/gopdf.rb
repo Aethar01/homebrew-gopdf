f751b69d838f2b95fdcff4272647dfbc20de192b
Elliott Ashby <elliott.ashby88@gmail.com>
2026-08-25 10:15:38 +0100

    Update gopdf version from 0.2.9 to 0.3.0

--- Formula/gopdf.rb
+++ Formula/gopdf.rb
@@ -2,7 +2,7 @@ class Gopdf < Formula
   desc "MuPDF-backend PDF viewer written in Go with Lua configuration"
   homepage "https://github.com/Aethar01/gopdf"
   url "https://github.com/Aethar01/gopdf.git",
-      tag:      "0.2.9"
+      tag:      "0.3.0"
   license "AGPL-3.0-only"
   head "https://github.com/Aethar01/gopdf.git", branch: "main"
 
