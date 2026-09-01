b3f4a84e8aa212f4e590e0501f5f2d27f90432f6
Elliott Ashby <elliott.ashby88@gmail.com>
2026-09-01 11:56:52 +0100

    Update gopdf to version 0.5.0 with new SHA256

--- Casks/gopdf.rb
+++ Casks/gopdf.rb
@@ -1,9 +1,9 @@
 cask "gopdf" do
   arch arm: "arm64", intel: "amd64"
 
-  version "0.4.0"
-  sha256 arm:   "536037d9694813ab5fef81ea7a40f091a20727f434067d50a4a3b9cc4d40a8f8",
-         intel: "21a513ac816d348fc37c1519c52df3b1fff9113363e6fce1ce73b4905519f8e9"
+  version "0.5.0"
+  sha256 arm:   "293829eca0e3f50a47bc7751ed0252fbb939dad6df1b9bfd5e0e89f498399bf3",
+         intel: "89300cd49ec48df96311a7109eeea846ee54aa2d87f7624f70ae635893896250"
 
   url "https://github.com/Aethar01/gopdf/releases/download/#{version}/gopdf-#{version}-darwin-#{arch}.dmg"
   name "GoPDF"
